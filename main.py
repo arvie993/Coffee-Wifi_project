c758523781c244fa63282f232e4bf9a35d4c8538
arvie993 <108028984+arvie99@users.noreply.github.com>
2026-01-04 23:00:44 -0500

    Move secret key to .env file for security

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, URLField
 from wtforms.validators import DataRequired, URL
+from dotenv import load_dotenv
 import csv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 Bootstrap5(app)
 
 
